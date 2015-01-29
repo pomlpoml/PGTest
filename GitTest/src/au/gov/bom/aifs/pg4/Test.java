3e8fefd0a17a500d7041e89204d4cc3507fbbc1a
pmemmel <p.memmel@bom.gov.au>
2015-01-30 09:21:09 +1100

    changed message

--- GitTest/src/au/gov/bom/aifs/pg4/Test.java
+++ GitTest/src/au/gov/bom/aifs/pg4/Test.java
@@ -2,7 +2,8 @@ package au.gov.bom.aifs.pg4;
 
 public class Test {
 	public static void main(String[] args){
-		System.out.println("g'day");
+		System.out.println("good day");
+		System.out.println("mate");
 	}
 
 }
